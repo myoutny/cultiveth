fb7f8a8ca70d646bd808a77377651e74642a90a9
myoutny <myoutny@gmail.com>
2021-05-29 14:58:17 -0700

    init changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,3 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional 
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
@@ -22,8 +17,6 @@ async function main() {
   console.log("Greeter deployed to:", greeter.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch(error => {
